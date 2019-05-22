c3f148f4774dcb9de55383cc2e37be2cede80f65
quanyujian <quanyujian@fshows.com>
2019-05-23 01:08:04 +0800

    'path'

--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://39.97.118.178:3000',
         changeOrigin: true,
         ws: true,
         pathRewrite: {
